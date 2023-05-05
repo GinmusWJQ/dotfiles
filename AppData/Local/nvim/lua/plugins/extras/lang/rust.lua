6f433ab0b467580bf8301afab6e87d513a810aa2
GinmusWJQ <104190153+GinmusWJQ@users.noreply.github.com>
2023-05-05 10:55:31 +0800

    修复rust在windows下不行调试的bug

--- AppData/Local/nvim/lua/plugins/extras/lang/rust.lua
+++ AppData/Local/nvim/lua/plugins/extras/lang/rust.lua
@@ -116,9 +116,17 @@ return {
             local codelldb = mason_registry.get_package("codelldb")
             local extension_path = codelldb:get_install_path() .. "/extension/"
             local codelldb_path = extension_path .. "adapter/codelldb"
-            local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-              or extension_path .. "lldb/lib/liblldb.so"
+            local liblldb_path = extension_path .. "lldb/lib/liblldb"
+            local this_os = vim.loop.os_uname().sysname
 
+            -- The path in windows is different
+            if this_os:find("Windows") then
+              codelldb_path = extension_path .. "adapter\\codelldb.exe"
+              liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+            else
+              -- The liblldb extension is .so for linux and .dylib for macOS
+              liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+            end
             rust_tools_opts = vim.tbl_deep_extend("force", rust_tools_opts, {
               dap = {
                 adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
