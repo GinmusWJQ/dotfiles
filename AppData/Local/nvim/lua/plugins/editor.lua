5c02edd3cd17aa7f770cf4f43fb48cc7fcba1340
GinmusWJQ <104190153+GinmusWJQ@users.noreply.github.com>
2023-05-05 10:06:48 +0800

    删除一些注释

--- AppData/Local/nvim/lua/plugins/editor.lua
+++ AppData/Local/nvim/lua/plugins/editor.lua
@@ -142,19 +142,10 @@ return {
   },
   {
     "folke/which-key.nvim",
-    -- opts = function(_, opts)
-    --   vim.list_extend(opts.defaults, {
-    --     ["<leader>r"] = { name = "rust actions", ["c"] = { name = "crate action" } },
-    --   })
-    -- end,
     opts = {
       defaults = {
         ["<leader>r"] = { name = "rust actions", ["c"] = { name = "crate action" } },
       },
     },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.register(opts.defaults)
-    end,
   },
 }
