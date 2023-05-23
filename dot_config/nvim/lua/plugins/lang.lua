4b37b5686768b7cf88cf8bfc352a5ba6629c8fa5
ginmus <wjq20010505@163.com>
2023-05-23 09:25:14 +0800

    添加markdown preview 执行command line 命令：MarkdownPreview即可

--- dot_config/nvim/lua/plugins/lang.lua
+++ dot_config/nvim/lua/plugins/lang.lua
@@ -30,4 +30,11 @@ return {
   -- { import = "plugins.extras.lang.java" },
   { import = "plugins.extras.lang.rust" },
   { import = "plugins.extras.lang.nodejs" },
+  {
+    "iamcco/markdown-preview.nvim",
+    event = "BufRead",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
